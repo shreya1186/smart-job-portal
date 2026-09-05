c7fefad26d7a9694d467c06b0a592ce6cdcfdf07
Shreya Prajapati <shreyaprajapati6002@gmail.com>
2026-09-06 00:18:47 +0530

    Add Vercel frontend to CORS configuration

--- smart-job-portal/src/main/java/com/smartjobportal/smart_job_portal/config/SecurityConfig.java
+++ smart-job-portal/src/main/java/com/smartjobportal/smart_job_portal/config/SecurityConfig.java
@@ -59,7 +59,8 @@ public class SecurityConfig {
 
         configuration.setAllowedOrigins(List.of(
                 "http://127.0.0.1:5500",
-                "http://localhost:5500"
+                "http://localhost:5500",
+                "https://smart-job-portal-frontend-one.vercel.app"
         ));
 
         configuration.setAllowedMethods(List.of(
